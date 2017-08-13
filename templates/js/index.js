45c0b948620681987c5f50cc2110a87d6917bec1
cloudIC <844033231@qq.com>
2017-08-13 17:43:52 +0800

    feat: 邮箱认证

--- templates/js/index.js
+++ templates/js/index.js
@@ -2,6 +2,7 @@
     var popup = $('.popup')
     var popClose = $('.pop-close')
     var subscribe = $('.btn-subscribe')
+    var eamil = $('.subscribe-input')
     var subscribeLink = $('.subscribe-link')
     subscribeLink.click(function() {
         $('html,body').animate({
@@ -16,7 +17,16 @@
     subscribe.click(function() {
         // to do
         // 后台交互
+        if (!isEmail(eamil.val())) {
+            alert('请输入输入正确的邮箱')
+            return;
+        }
         popup.fadeIn(200)
         popup.css('display', 'flex')
+        eamil.val('')
     })
+    function isEmail(str){
+        var reg = /^(\w)+(\.\w+)*@(\w)+((\.\w+)+)$/;
+        return reg.test(str);
+    }
 })()
