b6fe608e6ed867ff3f53f333db93c310b5eee3c9
cloudIC <844033231@qq.com>
2017-08-20 19:34:39 +0800

    finish

--- templates/js/index.js
+++ templates/js/index.js
@@ -1,9 +1,23 @@
 (function() {
     var popup = $('.popup')
+    var smsPop = popup.find('.pop-sms-wrap')
+    var bookPop = popup.find('.pop-booking-wrap')
     var popClose = $('.pop-close')
+    var popOk = $('.pop-ok')
     var subscribe = $('.btn-subscribe')
     var email = $('.subscribe-input')
     var subscribeLink = $('.subscribe-link')
+    var bookBtn = $('.btn-book')
+    
+    bookBtn.click(function(e) {
+        e.preventDefault()
+        e.stopPropagation()
+        smsPop.hide()
+        bookPop.show()
+        popup.fadeIn(200)
+        popup.css('display', 'flex')
+    })
+
     subscribeLink.click(function() {
         $('html,body').animate({
             scrollTop: subscribe.offset().top - 300
@@ -11,6 +25,9 @@
         subscribe.offset().top
     })
     popClose.click(function() {
+        popup.fadeOut(200)
+    })
+    popOk.click(function() {
         popup.fadeOut(200)
     })
 
@@ -26,6 +43,8 @@
         }
         $.post('dev/subscriber', { email: email.val() }, function(data) {
             if (data.success) {
+                smsPop.show()
+                bookPop.hide()
                 popup.fadeIn(200)
                 popup.css('display', 'flex')
                 email.val('')
